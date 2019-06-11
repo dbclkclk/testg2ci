eb0b8deb48f26a120eed72de88d25826366f0b65
deanclarke <dean.clarke@moduscreate.com>
2019-06-12 01:17:14 +0545

    fix issue with the production build - complete test

--- config/webpack.config.production.js
+++ config/webpack.config.production.js
@@ -1,51 +1,45 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const configuration = require('./webpack.config');
 
 module.exports = Object.assign({}, configuration, {
   mode: 'production',
-  module: {
-    rules:[
-       {
-            test: /\.css$/,
-            exclude: /node_modules/,
-            loader: [
-              MiniCssExtractPlugin.loader,
-              {
-                loader: 'css-loader',
-                options: {
-                  modules: true,
-                  localIdentName: '[name]__[local]___[hash:base64:5]',
-                  camelCase: true,
-                  sourceMap: true
-                }
-              },
-              {
-                loader: 'sass-loader',
-                options: {
-                  sourceMap: false
-                }
-              }
-            ]
+   module: {
+    rules: [
+          {
+                test: /\.css$/,
+                use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        hmr: false
+                    },
+                },
+                'css-loader',
+                ],
           },
           {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        loader: 'file-loader?name=[name].[ext]',
-      },
-      {
-        test: /\.js$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/,
-      }
+            test: /\.(eot|svg|ttf|woff|woff2)$/,
+            loader: 'file-loader?name=[name].[ext]',
+          },
+          {
+            test: /\.js$/,
+            use: ['babel-loader'],
+            exclude: /node_modules/,
+          }
     ]
   },
   plugins: [
     new CopyWebpackPlugin([{ from: 'public' }]),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css',
     }),
     new HtmlWebpackPlugin({ template: './public/index.html' }),
   ],
